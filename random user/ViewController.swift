ac5d2daae8b7af4a29c471e33291f288a03bcdea
Hsiao <rehsiao@gmail.com>
2020-07-26 15:35:18 +0800

    Initial Commit

---
+++ random user/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  random user
+//
+//  Created by H.W. Hsiao on 2020/7/26.
+//  Copyright © 2020 H.W. Hsiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
